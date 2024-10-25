86581deb6a6490114ff5d2c8ee578241476e4df1
Nicolas Guillon <nicolas.guillon@labom2iformation.fr>
2024-10-25 14:53:13 +0200

    ajout page inscription et route

--- Front/Easy_cook_angular/src/app/app.routes.ts
+++ Front/Easy_cook_angular/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { AccueilComponent } from './pages/accueil/accueil.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
 
 export const routes: Routes = [
     {path: "", component: AccueilComponent},
-    {path:"register", component: InscriptionComponent}
+    {path: "register", component: InscriptionComponent}
 ];
--- Front/Easy_cook_angular/src/app/pages/inscription/inscription.component.ts
+++ Front/Easy_cook_angular/src/app/pages/inscription/inscription.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.css'
 })
