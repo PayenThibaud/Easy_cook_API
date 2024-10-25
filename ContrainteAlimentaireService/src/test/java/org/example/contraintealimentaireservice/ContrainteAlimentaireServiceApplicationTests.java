f46efda178a513560a6612530a01d076e03c3674
PayenThibaud <Thibaudpayenbts@gmail.com>
2024-10-25 10:29:53 +0200

    ajout contrainte alimentaire et les ports du gateway

---
+++ ContrainteAlimentaireService/src/test/java/org/example/contraintealimentaireservice/ContrainteAlimentaireServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.contraintealimentaireservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContrainteAlimentaireServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
