fd6e8bcfb58c425b672b1179b29bebc45a681d4a
Nicolas Guillon <nicolas.guillon@labom2iformation.fr>
2024-10-28 16:20:34 +0100

    qjout html detail page

--- Front/Easy_cook_angular/src/app/app.routes.ts
+++ Front/Easy_cook_angular/src/app/app.routes.ts
@@ -4,10 +4,13 @@ import { AccueilComponent } from './pages/accueil/accueil.component';
 import { InscriptionComponent } from './pages/inscription/inscription.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ListeRecettesComponent } from './pages/liste-recettes/liste-recettes.component';
+import { DetailRecetteComponent } from './pages/detail-recette/detail-recette.component';
 
 export const routes: Routes = [
     {path: "", component: AccueilComponent},
     {path: "register", component: InscriptionComponent},
     {path: "login", component: LoginComponent},
-    {path: "recettes", component: ListeRecettesComponent}
+    {path: "recettes", component: ListeRecettesComponent},
+    // à adapter en fonction de l'ID recette
+    {path: "recette", component: DetailRecetteComponent},
 ];
