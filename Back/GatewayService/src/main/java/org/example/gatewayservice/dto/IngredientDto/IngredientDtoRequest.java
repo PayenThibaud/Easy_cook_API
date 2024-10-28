97d6c100449267ddfbb354284eeacce48f7f45e9
PayenThibaud <Thibaudpayenbts@gmail.com>
2024-10-28 10:43:27 +0100

    ajout dans le gateway Ingredient

---
+++ Back/GatewayService/src/main/java/org/example/gatewayservice/dto/IngredientDto/IngredientDtoRequest.java
@@ -0,0 +1,12 @@
+package org.example.gatewayservice.dto.IngredientDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IngredientDtoRequest {
+    private String nom;
+}
