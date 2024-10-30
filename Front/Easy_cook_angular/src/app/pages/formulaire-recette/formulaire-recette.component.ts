a74183568676bbdc5206560b59f8ddfb4d6fa632
Nicolas Guillon <nicolas.guillon@labom2iformation.fr>
2024-10-30 16:39:09 +0100

    ajout fonctions formulaire recette

--- Front/Easy_cook_angular/src/app/pages/formulaire-recette/formulaire-recette.component.ts
+++ Front/Easy_cook_angular/src/app/pages/formulaire-recette/formulaire-recette.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { ListeRecettesComponent } from '../liste-recettes/liste-recettes.component';
 
 @Component({
   selector: 'app-formulaire-recette',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, ListeRecettesComponent],
   templateUrl: './formulaire-recette.component.html',
   styleUrl: './formulaire-recette.component.css'
 })
 export class FormulaireRecetteComponent {
+
+  recipeForm= new FormGroup({
+    nom: new FormControl(""),
+    image: new FormControl(""),
+    duree: new FormControl(0),
+    calorie: new FormControl,
+    regime: new FormControl([]),
+    ingredients: new FormArray([
+      new FormGroup({
+        nom: new FormControl(""),
+        gramme: new FormControl(0),
+        litre: new FormControl(0),
+        quantite: new FormControl(0)
+      }),
+    ]),
+    etapes: new FormControl([]),
+    auteur: new FormControl("")
+  })
+
+  get ingredients() {
+    return this.recipeForm.controls.ingredients;
+  }
+
+  addIngredient(): void {
+    this.ingredients.push(
+      new FormGroup({
+        nom: new FormControl(""),
+        gramme: new FormControl(0),
+        litre: new FormControl(0),
+        quantite: new FormControl(0)
+      })
+    )
+  }
+
+  
 
 }
